38e690cbb2b5430643a75b5e8430a611e8789ee0
PEDRO HENRIQUE MIGUEL DA SILVA <psilva004@cs.unipe.edu.br>
2026-08-24 21:22:57 -0300

    Exercício 2

---
+++ Exercicio02/src/main/java/exercicio2/FuncionarioCLT.java
@@ -0,0 +1,18 @@
+package exercicio2;
+
+public class FuncionarioCLT extends Funcionario{
+	public double valeTransporte;
+	public double valeAlimentacao;
+
+	public FuncionarioCLT(String nome, String matricula, double salarioBase, String dataAdmissao, double valeTransporte, double valeAlimentacao) {
+		super(nome, matricula, salarioBase, dataAdmissao);
+		this.valeTransporte = valeTransporte;
+		this.valeAlimentacao = valeAlimentacao;
+	}
+
+	@Override
+	public double calcularSalario() {
+		return super.calcularSalario();
+	}
+	
+}
